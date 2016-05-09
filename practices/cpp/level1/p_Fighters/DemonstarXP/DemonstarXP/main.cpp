342e5a67521a7c8f4afdb7f69065fc0ccd5a75fd
= <=>
2016-05-09 15:08:18 +0800

    修改图像

--- practices/cpp/level1/p_Fighters/DemonstarXP/DemonstarXP/main.cpp
+++ practices/cpp/level1/p_Fighters/DemonstarXP/DemonstarXP/main.cpp
@@ -1,37 +1,34 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Audio/Music.hpp>
 int main()
 {
-    // create the window
-    sf::RenderWindow window(sf::VideoMode(1000, 800), "My window");
-
-    // run the program as long as the window is open
+    sf::RenderWindow window(sf::VideoMode(800, 900), "DemonstarXP");
     while (window.isOpen())
     {
-        // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
         while (window.pollEvent(event))
         {
-            // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-		// load a 32x32 rectangle that starts at (10, 10)
+
 		sf::Texture texture;
-		if (!texture.loadFromFile("image.png"))
-		{
-		    // error...
+		if (!texture.loadFromFile("background.jpg")){
 		}
 		sf::Sprite sprite;
 		sprite.setTexture(texture);
-		// position
-		sprite.setPosition(sf::Vector2f(400, 720)); // absolute position
-        // clear the window with black color
-        window.clear(sf::Color::Black);
-
-        // draw everything here...
-        //window.draw(...);
 		window.draw(sprite);
-        // end the current frame
+		// load a 32x32 rectangle that starts at (10, 10)
+		sf::Texture planes;
+		if (!planes.loadFromFile("image.png"))
+		{
+		}
+		sf::Sprite plane;
+		plane.setTexture(planes);
+		plane.setPosition(sf::Vector2f(360, 820));
+		window.draw(plane);
+
         window.display();
     }
 
